7aa7880124b11cc7719fc06d2ae36bb9ee7c1be3
datdnit <datdnk3@gmail.com>
2024-12-11 20:55:58 +0700

    base code

---
+++ tour-service/src/main/java/com/hau/tour_service/TourServiceApplication.java
@@ -0,0 +1,13 @@
+package com.hau.tour_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TourServiceApplication.class, args);
+	}
+
+}
