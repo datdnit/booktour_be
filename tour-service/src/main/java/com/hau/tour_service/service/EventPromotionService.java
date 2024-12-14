dc02872991d7fea5faad45e4fe48a6af53c86cca
phthuys <imthuys2003@gmail.com>
2024-12-14 22:10:27 +0700

    thuy commit

---
+++ tour-service/src/main/java/com/hau/tour_service/service/EventPromotionService.java
@@ -0,0 +1,33 @@
+package com.hau.tour_service.service;
+
+import com.hau.tour_service.entity.EventPromotion;
+import com.hau.tour_service.repo.EventPromotionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+
+@Service
+public class EventPromotionService {
+
+    @Autowired
+    private EventPromotionRepository repository;
+
+    public List<EventPromotion> findAll() {
+        return repository.findAll();
+    }
+
+    public Optional<EventPromotion> findById(Long id) {
+        return repository.findById(id);
+    }
+
+    public EventPromotion save(EventPromotion eventPromotion) {
+        return repository.save(eventPromotion);
+    }
+
+    public void deleteById(Long id) {
+        repository.deleteById(id);
+    }
+}
